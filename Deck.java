1de632bbcd917260aca430352423308e6a44554b
prishajaiswal75 <164599457+prishajaiswal75@users.noreply.github.com>
2025-03-04 18:50:00 +0530

    Create A deak of 52 Cards.java

--- Deck.java
+++ Deck.java
@@ -8,3 +8,16 @@ class Deck {
         deck = new ArrayList<>();
         createDeck();
     }
+
+    // Creates a deck of 52 cards
+    public void createDeck() {
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+        for (String rank : ranks) {
+            for (String suit : suits) {
+                deck.add(new Card(rank, suit));
+            }
+        }
+    }
+
