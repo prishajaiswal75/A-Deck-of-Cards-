2b1f2bf856ad11e8caf1f55aec9f68ea70ceece1
prishajaiswal75 <164599457+prishajaiswal75@users.noreply.github.com>
2025-03-04 18:56:37 +0530

    Constructors

---
+++ Card.java
@@ -0,0 +1,9 @@
+class Card {
+    private String rank;
+    private String suit;
+
+    // Constructor
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
